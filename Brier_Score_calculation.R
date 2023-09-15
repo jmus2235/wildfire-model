880bd5b0669c3ee0806f4102f37e2ee400aea518
John Musinsky <jmusinsky@battelleecology.org>
2023-09-15 12:40:25 -0600

    Updated version of Brier_Score_calculation.R

--- Brier_Score_calculation.R
+++ Brier_Score_calculation.R
@@ -2,13 +2,14 @@
 
 # Load the DescTools package
 library(DescTools)
+library(ModelMetrics)
 
 # set working directory
 wd <- "~/R_Scripts/NEON-Climate-variable-validation/data" 
 setwd(wd)
 
 # Read csv file
-df <- read.csv("Validated_Fire_No_Fire_Sample_Points_2019_seed_2.csv", stringsAsFactors = FALSE)
+df <- read.csv("Validated_Fire_No_Fire_Sample_Points_2019_seed_2_no-ws.csv", stringsAsFactors = FALSE)
 
 # Define your predicted probabilities and observed outcomes
 predicted_probabilities <- df$classification
@@ -20,6 +21,10 @@ brier_score_desc <- BrierScore(observed_outcomes, predicted_probabilities)
 # Using Manual Calculation
 brier_score_manual <- mean((predicted_probabilities - observed_outcomes)^2)
 
+# Log-loss using ModelMetrics
+logloss_modelmetric <- logLoss(observed_outcomes, predicted_probabilities)
+
 # Print both Brier Scores
 print(brier_score_desc)
 print(brier_score_manual)
+print(logloss_modelmetric)
